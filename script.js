f9a7fb193f9662ae0edd6d2ca847dfd3e5e6c15b
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-01-21 03:03:40 -0500

    carousel added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const sentences = ["Passionate web developer with expertise in Python, HTML, CSS, and JavaScript.","Dedicated to continuous learning.","Crafting exceptional digital experiences through self-taught proficiency."];
 let typingElement = document.querySelector('#web-dev'); // typing effect div
+const glide = document.querySelector('.glide');
+
+// typewriter
 var typewriter = new Typewriter(typingElement, {
   loop: true,
   autoStart: true,
@@ -11,4 +14,12 @@ typewriter.typeString(sentence)
     .deleteAll(40)
     .start()
 }
+// slider
+new Glide(glide,{
+animationDuration: 1000,
+autoplay:1500,
+type: 'carousel',
+startAt: 1,
+perView: 3,
 
+}).mount()
